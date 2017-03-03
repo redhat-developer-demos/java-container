2dc19d46610498b090c164f18435e1696f68d0d3
Rafael Benevides <rafabene@gmail.com>
2017-03-03 16:48:28 -0500

    Added a new image

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,3 @@
 docker build -t rafabene/java-container:openjdk -f Dockerfile.openjdk .
 docker build -t rafabene/java-container:openjdk-env -f Dockerfile.openjdk-env .
+docker build -t rafabene/java-container:fabric8 -f Dockerfile.fabric8 .
