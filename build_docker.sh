d8967d3a009cc7e86b477802fde19e23be346687
Rafael Benevides <rafabene@gmail.com>
2018-03-15 21:59:27 -0400

    Updated to JDK 9 and created a new version

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,4 @@
 docker build -t rafabene/java-container:openjdk -f Dockerfile.openjdk .
 docker build -t rafabene/java-container:openjdk-env -f Dockerfile.openjdk-env .
+docker build -t rafabene/java-container:openjdk-cgroup -f Dockerfile.openjdk-cgroup .
 docker build -t rafabene/java-container:fabric8 -f Dockerfile.fabric8 .
