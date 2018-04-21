6cfca793b7847510ba653b3e57db8d6e1832b499
Rafael Benevides <rafabene@gmail.com>
2018-04-21 16:08:05 -0300

    Added JDK 10

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,5 @@
 docker build -t rafabene/java-container:openjdk -f Dockerfile.openjdk .
+docker build -t rafabene/java-container:openjdk10 -f Dockerfile.openjdk10 .
 docker build -t rafabene/java-container:openjdk-env -f Dockerfile.openjdk-env .
 docker build -t rafabene/java-container:openjdk-cgroup -f Dockerfile.openjdk-cgroup .
 docker build -t rafabene/java-container:fabric8 -f Dockerfile.fabric8 .
