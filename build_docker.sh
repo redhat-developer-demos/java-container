955272603919e1e2438fcd9f76564f03f6b3c8f5
Rafael Benevides <rafabene@gmail.com>
2019-04-26 15:37:03 -0300

    Update to latest versions

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,5 @@
 docker build -t rafabene/java-container:openjdk -f Dockerfile.openjdk .
-docker build -t rafabene/java-container:openjdk10 -f Dockerfile.openjdk10 .
+docker build -t rafabene/java-container:openjdk12 -f Dockerfile.openjdk12 .
 docker build -t rafabene/java-container:openjdk-env -f Dockerfile.openjdk-env .
 docker build -t rafabene/java-container:openjdk-cgroup -f Dockerfile.openjdk-cgroup .
 docker build -t rafabene/java-container:fabric8 -f Dockerfile.fabric8 .
