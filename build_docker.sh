cc46743adc08086e47e5b54830f5819bbbf5d324
Rafael Benevides <rafabene@gmail.com>
2019-05-17 20:13:40 -0400

    Renamed jdk 9 files

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,5 @@
 mvn clean package -DskipTests
-docker build -t rafabene/java-container:openjdk -f Dockerfile.openjdk .
+docker build -t rafabene/java-container:openjdk9 -f Dockerfile.openjdk9 .
 docker build -t rafabene/java-container:openjdk8 -f Dockerfile.openjdk8 .
 docker build -t rafabene/java-container:openjdk12 -f Dockerfile.openjdk12 .
 docker build -t rafabene/java-container:openjdk-env -f Dockerfile.openjdk-env .
